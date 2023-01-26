1c7bf9c86df2052515fbcbb717aa94b03bd42330
Marcelo Fortunato <mp.fortunato88@gmail.com>
2023-01-26 21:04:23 +0000

    Initial commit

---
+++ apps/nextjs-app/pages/index.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function IndexPage() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Link href="/api/users">View Users</Link>
+    </div>
+  );
+}
